ebabf2bdcea441a438519130ceb3662ad260d164
schert <35879052+schert@users.noreply.github.com>
2022-01-31 12:49:31 +0100

    removed set get

--- gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/model/BenBeneficiario.java
+++ gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/model/BenBeneficiario.java
@@ -2,6 +2,10 @@ package gov.mef.gfa.anagrafiche.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 
@@ -9,6 +13,8 @@ import java.math.BigDecimal;
  * The persistent class for the BEN_BENEFICIARIO database table.
  * 
  */
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name="BEN_BENEFICIARIO")
 public class BenBeneficiario implements Serializable {
@@ -47,96 +53,5 @@ public class BenBeneficiario implements Serializable {
 	private String telefono;
 
 	private BigDecimal tipopersona;
-
-	public BenBeneficiario() {
-	}
-
-	public String getCodicefiscale() {
-		return this.codicefiscale;
-	}
-
-	public void setCodicefiscale(String codicefiscale) {
-		this.codicefiscale = codicefiscale;
-	}
-
-	public String getEmail() {
-		return this.email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getFax() {
-		return this.fax;
-	}
-
-	public void setFax(String fax) {
-		this.fax = fax;
-	}
-
-	public BigDecimal getIdbeneficiario() {
-		return this.idbeneficiario;
-	}
-
-	public void setIdbeneficiario(BigDecimal idbeneficiario) {
-		this.idbeneficiario = idbeneficiario;
-	}
-
-	public String getIndirizzo() {
-		return this.indirizzo;
-	}
-
-	public void setIndirizzo(String indirizzo) {
-		this.indirizzo = indirizzo;
-	}
-
-	public String getNominativo() {
-		return this.nominativo;
-	}
-
-	public void setNominativo(String nominativo) {
-		this.nominativo = nominativo;
-	}
-
-	public String getNote() {
-		return this.note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
-	}
-
-	public String getPartitaiva() {
-		return this.partitaiva;
-	}
-
-	public void setPartitaiva(String partitaiva) {
-		this.partitaiva = partitaiva;
-	}
-
-	public String getRagionesociale() {
-		return this.ragionesociale;
-	}
-
-	public void setRagionesociale(String ragionesociale) {
-		this.ragionesociale = ragionesociale;
-	}
-
-	public String getTelefono() {
-		return this.telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public BigDecimal getTipopersona() {
-		return this.tipopersona;
-	}
-
-	public void setTipopersona(BigDecimal tipopersona) {
-		this.tipopersona = tipopersona;
-	}
 
 }
--- gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/model/Convenzioni.java
+++ gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/model/Convenzioni.java
@@ -2,6 +2,10 @@ package gov.mef.gfa.anagrafiche.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 
@@ -9,6 +13,8 @@ import java.math.BigDecimal;
  * The persistent class for the CONVENZIONI database table.
  * 
  */
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name="CONVENZIONI")
 public class Convenzioni implements Serializable {
@@ -38,64 +44,5 @@ public class Convenzioni implements Serializable {
 
 	@Column(nullable=false, precision=12, scale=2)
 	private BigDecimal somma;
-
-	public Convenzioni() {
-	}
-
-	public BigDecimal getIdattivita() {
-		return this.idattivita;
-	}
-
-	public void setIdattivita(BigDecimal idattivita) {
-		this.idattivita = idattivita;
-	}
-
-	public String getIndirizzo() {
-		return this.indirizzo;
-	}
-
-	public void setIndirizzo(String indirizzo) {
-		this.indirizzo = indirizzo;
-	}
-
-	public BigDecimal getMaxfinerogabile() {
-		return this.maxfinerogabile;
-	}
-
-	public void setMaxfinerogabile(BigDecimal maxfinerogabile) {
-		this.maxfinerogabile = maxfinerogabile;
-	}
-
-	public BigDecimal getModificato() {
-		return this.modificato;
-	}
-
-	public void setModificato(BigDecimal modificato) {
-		this.modificato = modificato;
-	}
-
-	public BigDecimal getMoltiplicatore() {
-		return this.moltiplicatore;
-	}
-
-	public void setMoltiplicatore(BigDecimal moltiplicatore) {
-		this.moltiplicatore = moltiplicatore;
-	}
-
-	public String getNomeistituto() {
-		return this.nomeistituto;
-	}
-
-	public void setNomeistituto(String nomeistituto) {
-		this.nomeistituto = nomeistituto;
-	}
-
-	public BigDecimal getSomma() {
-		return this.somma;
-	}
-
-	public void setSomma(BigDecimal somma) {
-		this.somma = somma;
-	}
 
 }
