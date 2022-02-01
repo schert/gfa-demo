9f04db8613e272c0092726cf7d04c65a901edf10
gvolponi <gvolponi@BIPGVOLPONI01.mshome.net>
2022-02-01 10:10:54 +0100

    add Gian Marco test

---
+++ gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/dao/BenStoricoDao.java
@@ -0,0 +1,13 @@
+package gov.mef.gfa.anagrafiche.dao;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gov.mef.gfa.anagrafiche.model.BenStoricoSettore;
+
+@Repository
+public interface BenStoricoDao extends JpaRepository<BenStoricoSettore, BigDecimal> {
+	public BenStoricoSettore findByidbeneficiario(BigDecimal id);
+}
--- gfa-anagrafiche/src/test/java/gov/mef/gfa/anagrafiche/GfaFeApplicationTests.java
+++ gfa-anagrafiche/src/test/java/gov/mef/gfa/anagrafiche/GfaFeApplicationTests.java
@@ -1,13 +1,30 @@
 package gov.mef.gfa.anagrafiche;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import gov.mef.gfa.anagrafiche.dao.BenStoricoDao;
+import gov.mef.gfa.anagrafiche.model.BenStoricoSettore;
 
 @SpringBootTest(classes = GfaAnagraficheApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class GfaBeApplicationTests {
+	
+	@Autowired
+	BenStoricoDao benStoricoDao;
 
 	@Test
 	public void testGetAllUsers() {
-
+		BenStoricoSettore storico = new BenStoricoSettore();
+		storico.setIdbeneficiario(new BigDecimal(1));
+		storico.setIdsettore("1");
+		storico.setAnnocompetenzada(new BigDecimal(2022));
+		storico.setAnnocompetenzaa(new BigDecimal(2023));
+		
+		benStoricoDao.save(storico);
+		benStoricoDao.findAll();
+		benStoricoDao.delete(storico);
 	}
 }
