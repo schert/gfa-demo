eaac9d22ca835149e2a88a3652fc9ea1ef0f9f39
schert <35879052+schert@users.noreply.github.com>
2022-02-05 01:35:38 +0100

    standard timezone

--- gfa-common/src/main/java/gov/mef/gfa/common/utils/MapperUtils.java
+++ gfa-common/src/main/java/gov/mef/gfa/common/utils/MapperUtils.java
@@ -6,7 +6,7 @@ import com.google.gson.GsonBuilder;
 public class MapperUtils {
 	
 	public static <T,E> T copyProperties(E from, Class<T> to) {
-		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss.S Z").create();			
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SZ").create();			
 		return gson.fromJson(gson.toJson(from), to);
 	}
 
