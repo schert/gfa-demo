568d2e5009ceedd8278d09e3e1eaf7fcfd95570a
schert <35879052+schert@users.noreply.github.com>
2022-02-14 18:13:06 +0100

    rimosso import inutilizzato

--- gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/service/impl/FinanziamentoDirettoImpl.java
+++ gfa-anagrafiche/src/main/java/gov/mef/gfa/anagrafiche/service/impl/FinanziamentoDirettoImpl.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import gov.mef.gfa.anagrafiche.controller.FinanziamentoDirettoController;
 import gov.mef.gfa.anagrafiche.dao.FinanziamentoDirettoDAO;
 import gov.mef.gfa.anagrafiche.exception.ServiceException;
 import gov.mef.gfa.anagrafiche.model.FinanziamentoDiretto;
